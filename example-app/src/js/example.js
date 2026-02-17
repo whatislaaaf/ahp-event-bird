e5733620bccf4409c52dc6827badcbd0e191b11f
Vlad M <whatislaaaf@gmail.com>
2026-02-17 11:38:02 +0500

    first commit

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { AhpEventBird } from 'ahp-event-bird';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    AhpEventBird.echo({ value: inputValue })
-}
--- ios/Sources/AhpEventBirdPlugin/AhpEventBird.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-@objc public class AhpEventBird: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
-    }
-}
